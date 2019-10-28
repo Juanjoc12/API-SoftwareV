c1ca789a02db7a219dbe0ded33737ffaf6c9e5d5
Juanjoc12 <coche-9@hotmail.com>
2019-10-28 10:19:04 -0500

    Coordenadas mejoradas

--- src/routes/rutas.js
+++ src/routes/rutas.js
@@ -1,46 +1,49 @@
-const { Router } = require('express');
-const router = Router();
-const _ = require('underscore');
+let { Router } = require('express');
+let router = Router();
+let _ = require('underscore');
 
-const bd = require('../bd.json');
+let bd = require('../bd.json');
 
 router.get('/', (req,res) => {
     res.json(bd);
 });
 
 router.get('/:barrio', (req,res) => {
-    const barrios = bd.filter(function(estudiantes){
+    let barrios = bd.filter(function(estudiantes){
         return estudiantes.barrio ==  req.params.barrio;
     })
     res.json(barrios);
 });
 
 router.get('/estadisticas/barrio', (req, res) => {
-    const barrios = [...new Set(bd.map(dato => dato.barrio))]
-    const ninosBarrio = barrios.map(barrio => ({
+    let barrios = [...new Set(bd.map(dato => dato.barrio))]
+    let ninosBarrio = barrios.map(barrio => ({
         barrio, cantidad: (bd.filter(est => est.barrio === barrio)).length
     }))
     res.json(ninosBarrio);
 });
 
-router.get('/coordenadas/estudiante', (req, res) => {
-    const coordenada = bd.map(dato => ({
-        nombre: dato.nombre, 
-        latitud: dato.latitud,
-        longitud: dato.longitud
-    }))
+router.get('/coordenadas/:id', (req, res) => {
+    let id = bd.filter(function(estudiantes){
+        return estudiantes.id ==  req.params.id;
+    });
+    let coordenada = {
+      'nombre': id[0].nombre,
+      'latitud': id[0].latitud,
+      'longitud': id[0].longitud
+     }
     res.json(coordenada);
 });
 
 
 router.post('/', (req, res) => {
-    const { id,nombre,padre, madre,barrio,telefono } = req.body;
+    let { id,nombre,padre, madre,barrio,telefono } = req.body;
     if(id && nombre && padre && madre && barrio && telefono){
         _.each(bd, (dato, i)=>{
             if(dato.id == id){
                 res.json({error: 'Este usuario ya existe'});
             }else{
-                const nuevoNiño = {...req.body, id};
+                let nuevoNiño = {...req.body, id};
                 bd.push(nuevoNiño);  
             }
             res.json(bd); 
@@ -51,8 +54,8 @@ router.post('/', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-    const {id} = req.params;
-    const { barrio,telefono } = req.body;
+    let {id} = req.params;
+    let { barrio,telefono } = req.body;
     if(barrio && telefono){
         _.each(bd, (dato, i) => {
             if(dato.id == id){
@@ -67,7 +70,7 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id',(req,res) =>{
-    const {id} = req.params;
+    let {id} = req.params;
     _.each(bd, (dato, i)=>{
         if(dato.id == id){
             bd.splice(i,1);
